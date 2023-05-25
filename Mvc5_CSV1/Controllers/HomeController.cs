5a39e7d6ebacbc69f08eed0d81d7ebe55a84c498
Lucas Camilo <lucascamilo326@gmail.com>
2023-05-25 19:31:44 -0300

    Falta o input dividido

---
+++ Mvc5_CSV1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System.Data;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5_CSV1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
+
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        Stream stream = upload.InputStream;
+                        DataTable csvTable = new DataTable();
+                        using (CsvReader csvReader =
+                            new CsvReader(new StreamReader(stream), true))
+                        {
+                            csvTable.Load(csvReader);
+                        }
+                        return View(csvTable);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Arquivo", "Formato do arquivo não é suportado");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Arquivo", "Faça o Upload do arquivo");
+                }
+            }
+            return View();
+        }
+    }
+}
